f970a365ac08eb72e95908ea9ba9ce7a34f03a37
Avi <avishkardixita@gmail.com>
2022-11-13 02:26:16 +0530

    new folder 'multitreading practice' is added

---
+++ multitreading practice/src/prob1.java
@@ -0,0 +1,59 @@
+//Q1) print good morning and welcome repeatedly using tread
+//Q2) make welcome sleep by sleep method
+
+
+
+class welc extends Thread
+{
+    @Override
+    public void run() {
+
+        while(true)
+        {
+            System.out.println("welcome");
+        }
+    }
+}
+
+
+class gm extends Thread
+{
+    @Override
+    public void run() {
+
+        while(true)
+        {
+            System.out.println("good morning");
+        }
+    }
+}
+
+
+
+
+public class prob1
+{
+    public static void main(String[] args)
+    {
+        welc w1 = new welc();
+        w1.start();
+
+        //created sleep method in try and catch block
+        try {
+            w1.sleep(200);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+
+        gm g1 = new gm();
+        g1.start();
+        g1.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("priority of g1 :"+g1.getPriority());
+        System.out.println("priority of welc :"+w1.getPriority());
+
+
+    }
+}
